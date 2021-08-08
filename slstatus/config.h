8ccc9a51618f9ba8621a2c281f42b95cb153085b
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-08-08 05:45:10 -0500

    separate icons and added colors

--- slstatus/config.h
+++ slstatus/config.h
@@ -63,18 +63,34 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 
-/*	I'm using scripts from my personal-dot-files repo  
+/*	I'm using scripts from my personal-dot-files repo
 	just download them, make them executable and put them in /usr/local bin		*/
 
 	/* function format          argument */
-	{ run_command,	"%2s |", "cpu.sh" },
-    	{ run_command,	"%2s |", "memory.sh" },
-    	{ run_command,	"%2s |", "hdd.sh" },
-	{ run_command,	"%2s |", "network.sh" },
-	{ run_command,	"%s |", "vol.sh " },
-	{ run_command,	"%10s ", "weatherbar.sh " },
-	{ run_command,	"%2s |", "clock.sh" },
-	
+	{ run_command,	"^c#cc6666^ %2s", "cpuicon.sh " },
+	{ cpu_perc,	"^c#c5c8c6^ %s%% ", NULL },
+
+   	{ run_command,	"^c#b5bd68^ %2s ", "memicon.sh"  },
+	{ ram_perc,	"^c#c8c5c6^ %s%% ", NULL },
+
+   	{ run_command,	"^c#f0c674^ %2s ", "homeicon.sh" },
+/*  { run_command,	"^c#c5c8c6^ %2s ", "hdd.sh" },   */
+	{ disk_perc,	"^c#c5c8c6^ %s%% ", "/home/mike/" },
+
+	{ run_command,	"^c#81a2be^ %2s ", "neticon.sh" },
+	{ run_command,	"^c#c5c8c6^ %2s ", "ip.sh" },
+	{ run_command,	"^c#cc6666^ %2s ", "vpnicon.sh" },
+
+    { run_command,	"^c#85678f^ %s ", "volicon.sh " },
+	{ run_command,	"^c#c5c8c6^ %s ", "volume.sh " },
+
+	{ run_command,	"^c#c5c8c6^ %2s ", "forecast.sh " },
+	{ run_command,	"^c#cc6666^ %2s ", "tempicon.sh " },
+	{ run_command,	"^c#c5c8c6^ %2s ", "temp.sh " },
+
+
+	{ run_command,	"^c#c5c8c6^ %2s ", "clock.sh" },
+
 /*	{ cpu_perc,	" %s%% |", NULL },
 	{ ram_perc,	" %s%% |", NULL },
 	{ disk_perc,	" %s%% |", "/home/mike/" },
@@ -84,6 +100,6 @@ static const struct arg args[] = {
 	{ run_command,	" %2s ", "sed 20q ~/.config/weather.txt | grep value | awk '{print $2 $3}' | sed 's/\"//g' " },
 	{ run_command,	"%2s |", "grep temp_F ~/.config/weather.txt | awk '{print $2}' | sed 's/\"//g' | sed 's/,/F/g' " },
 	{ datetime, "%s",           "%a, %b  %d %R" }, */
-	
+
 
 };
