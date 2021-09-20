de33a93a794a9a5a7231da37f27c35ac75fad28e
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-09-19 23:42:15 -0500

    moved config to config.def.h

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -62,6 +62,33 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 static const struct arg args[] = {
+
+/*	I'm using scripts from my personal-dot-files repo
+	just download them, make them executable and put them in /usr/local bin		*/
+
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ run_command,	"^c#cc6666^ %2s", "cpuicon.sh " },
+	{ cpu_perc,	"^c#c5c8c6^ %s%% ", NULL },
+
+   	{ run_command,	"^c#b5bd68^ %2s ", "memicon.sh"  },
+	{ ram_perc,	"^c#c8c5c6^ %s%% ", NULL },
+
+   	{ run_command,	"^c#f0c674^ %2s ", "homeicon.sh" },
+/*  { run_command,	"^c#c5c8c6^ %2s ", "hdd.sh" },   */
+	{ disk_perc,	"^c#c5c8c6^ %s%% ", "/home/mike/" },
+
+	{ run_command,	"^c#81a2be^ %2s ", "neticon.sh" },
+	{ run_command,	"^c#c5c8c6^ %2s ", "ip.sh" },
+	{ run_command,	"^c#cc6666^ %2s ", "vpnicon.sh" },
+
+    { run_command,	"^c#85678f^ %s ", "volicon.sh " },
+	{ run_command,	"^c#c5c8c6^ %s ", "volume.sh " },
+
+	{ run_command,	"^c#c5c8c6^ %2s ", "forecast.sh " },
+	{ run_command,	"^c#cc6666^ %2s ", "tempicon.sh " },
+	{ run_command,	"^c#c5c8c6^ %2s ", "temp.sh " },
+
+
+	{ run_command,	"^c#c5c8c6^ %2s ", "clock.sh" },
+
 };
